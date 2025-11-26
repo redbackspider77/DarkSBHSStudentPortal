ffab66090d43887a800570d1d4cb6572b71c296c
redbackspider <66238571+redbackspider77@users.noreply.github.com>
2025-11-26 15:10:06 +1100

    Update fussyDownArrows.js

--- scripts/fussyDownArrows.js
+++ scripts/fussyDownArrows.js
@@ -1,7 +1,7 @@
+/* to do: finish*/
+
 panelTexts = document.querySelectorAll('.jpane-toggler span'); 
 
 Array.from(panelTexts).forEach(e => {
-    e.innerHTML = "<b style='color: #698BD8;'>▼</b> &nbsp;&nbsp;&nbsp;" + e.innerHTML
+    e.innerHTML = '<b id="toggler" style="color: #698BD8;"">▼</b> &nbsp;&nbsp;&nbsp;' + e.innerHTML
 });
-
-/* to do: add to manifest and finish this to replace with ▼s */
