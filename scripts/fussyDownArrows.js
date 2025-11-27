9e8c376abf6cb7bd839935f75dd7a786bb39887a
redbackspider <66238571+redbackspider77@users.noreply.github.com>
2025-11-27 17:07:40 +1100

    progressing

--- scripts/fussyDownArrows.js
+++ scripts/fussyDownArrows.js
@@ -1,7 +1,13 @@
-/* to do: finish*/
+/* to do: finish
 
 panelTexts = document.querySelectorAll('.jpane-toggler span'); 
 
 Array.from(panelTexts).forEach(e => {
-    e.innerHTML = '<b id="toggler" style="color: #698BD8;"">▼</b> &nbsp;&nbsp;&nbsp;' + e.innerHTML
+    e.innerHTML = '<b id="toggler" style="color: #698BD8;"">▼</b> &nbsp;&nbsp;&nbsp;' + e.innerHTML;
 });
+
+panels = document.querySelectorAll('.panel');
+
+Array.from(panels).forEach(panel => {
+    panel.addEventListener('click', function() {
+});*/
